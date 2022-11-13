d939a284cc6faa46cf1786b602e5c55aee879eed
Ömer Faruk Bulut <omerfarukbulut@192.168.1.37>
2022-11-13 22:06:01 +0300

    Commit message

--- Java Project/hello.java
+++ Java Project/hello.java
@@ -1,7 +1,7 @@
 // create a java project to print hello world
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
         //test
     }
 }
