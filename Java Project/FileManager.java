e168e1be95596b61b5b68e988f6f08296ab1d247
ceerdonmez <117481993+ceerdonmez@users.noreply.github.com>
2022-11-15 00:33:48 +0300

    Update FileManager.java

--- Java Project/FileManager.java
+++ Java Project/FileManager.java
@@ -18,7 +18,7 @@ public class FileManager {
 		 parser = new JSONParser();
 	}
 
-	/*public ArrayList<Student> readStudent(String path) {
+	public ArrayList<Student> readStudent(String path) {
 
 		ArrayList<Student> students = new ArrayList<Student>();
 
@@ -27,13 +27,16 @@ public class FileManager {
 
 			 JSONObject jsonObject = (JSONObject) obj;
 
-			String studentID = (String) jsonObject.get("studentID");
-
 			String studentName = (String) jsonObject.get("studentName");
-
 			String studentSurname = (String) jsonObject.get("Studentsurname");
 			JSONObject rawJSON = (JSONObject) jsonObject.get("transcript");
 			Transcript transcript = (Transcript) initializeTranscript(rawJSON);
+			rawJSON = (JSONObject) jsonObject.get("semester");
+			Semester semester = (Semester) initializeSemester(rawJSON);
+			rawJSON = (JSONObject) jsonObject.get("schedule");
+			Schedule schedule = (Schedule) initializeSchedule(rawJSON);
+			rawJSON = (JSONObject) jsonObject.get("advisor");
+			Advisor advisor = (Advisor) initializeAdvisor(rawJSON);
 
 			//Advisor advisor = (Advisor) jsonObject.get("advisor");
 
@@ -43,7 +46,7 @@ public class FileManager {
 
 			String enrolledYear = (String) jsonObject.get("enrolledYear");
 
-			students.add(new Student(studentID, studentName, studentSurname,enrolledYear));
+			students.add(new Student(studentName, studentSurname, transcript, semester, schedule, advisor, enrolledYear));
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -54,7 +57,115 @@ public class FileManager {
 		}
 
 		return students;
-	}*/
+	}
+	
+	public ArrayList<Course> readCourse(String path) {
+
+		ArrayList<Course> courses = new ArrayList<Course>();
+
+		try {
+			Object obj = parser.parse(new FileReader(path));
+
+			 JSONObject jsonObject = (JSONObject) obj;
+
+			String courseCode = (String) jsonObject.get("courseCode");
+			String courseName = (String) jsonObject.get("name");
+			Integer credit = (Integer) jsonObject.get("credit");
+			ArrayList<Course> prerequisites = (ArrayList<Course>) jsonObject.get("prerequisites");
+			JSONObject rawJSON = (JSONObject) jsonObject.get("semester");
+			Semester semester = (Semester) initializeSemester(rawJSON);
+			rawJSON = (JSONObject) jsonObject.get("lecturer");
+			Lecturer lecturer = (Lecturer) initializeLecturer(rawJSON);
+			ArrayList<Student> courseStudents = (ArrayList<Student>) jsonObject.get("courseStudents");
+			
+
+			//Advisor advisor = (Advisor) jsonObject.get("advisor");
+
+			//Semester semester = (Semester) jsonObject.get("semester");
+
+			//Schedule schedule = (Schedule) jsonObject.get("schedule");
+
+			courses.add(new Course(courseCode, courseName, credit, prerequisites, lecturer, semester, courseStudents));
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return courses;
+	}
+	
+	public ArrayList<Lecturer> readLecturer(String path) {
+
+		ArrayList<Lecturer> lecturers = new ArrayList<Lecturer>();
+
+		try {
+			Object obj = parser.parse(new FileReader(path));
+
+			 JSONObject jsonObject = (JSONObject) obj;
+
+			String lecturerName = (String) jsonObject.get("name");
+			String lecturerSurname = (String) jsonObject.get("surname");
+			JSONObject rawJSON = (JSONObject) jsonObject.get("schedule");
+			Schedule schedule = (Schedule) initializeSchedule(rawJSON);
+			ArrayList<Course> givenCourses = (ArrayList<Course>) jsonObject.get("givenCourses");
+			
+
+			//Advisor advisor = (Advisor) jsonObject.get("advisor");
+
+			//Semester semester = (Semester) jsonObject.get("semester");
+
+			//Schedule schedule = (Schedule) jsonObject.get("schedule");
+
+			lecturers.add(new Lecturer(lecturerName, lecturerSurname, schedule, givenCourses));
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return lecturers;
+	}
+	
+	public ArrayList<Advisor> readAdvisor(String path) {
+
+		ArrayList<Advisor> advisors = new ArrayList<Advisor>();
+
+		try {
+			Object obj = parser.parse(new FileReader(path));
+
+			 JSONObject jsonObject = (JSONObject) obj;
+
+			ArrayList<Student> advisedStudents = (ArrayList<Student>) jsonObject.get("advisedStudents");
+			String advisorName = (String) jsonObject.get("courseCode");
+			String advisorSurname = (String) jsonObject.get("name");
+			
+
+			//Advisor advisor = (Advisor) jsonObject.get("advisor");
+
+			//Semester semester = (Semester) jsonObject.get("semester");
+
+			//Schedule schedule = (Schedule) jsonObject.get("schedule");
+
+			advisors.add(new Advisor(advisedStudents, advisorName, advisorSurname));
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return advisors;
+	}
+	
 	public HashMap jsonToMap(String json) {
 		HashMap map = new Gson().fromJson(json, HashMap.class);
 		return map;
@@ -81,9 +192,7 @@ public class FileManager {
 		return new Gson().fromJson(json, Advisor.class);
 	}
 
-	public ArrayList<Student> readStudent(JSONObject jsonObject){
-		
-		ArrayList<Student> students = new ArrayList<Student>();
+	public Student initializeStudent(JSONObject jsonObject){
 		
 		Gson gson = new Gson();
 		String studentName = jsonObject.get("name").toString();
@@ -94,7 +203,52 @@ public class FileManager {
 		Advisor advisor = jsonToClassAdvisor(jsonObject.get("advisor").toString());
 		String enrolledYear = jsonObject.get("enrolledyear").toString();
 
-		return students.add(new Student(courseGrade,student,GPA,takenCredit,takenCourses,notTakenCourses));
+		return new Student(studentName,studentSurname,transcript,takenCredit,takenCourses,notTakenCourses));
+
+	}
+	
+	public Semester initializeSemester(JSONObject jsonObject){
+		
+		Gson gson = new Gson();
+		String semesterName = jsonObject.get("name").toString();
+		Type courseListType = new TypeToken<ArrayList<Course>>(){}.getType();
+		ArrayList<Course> openedCourse = gson.fromJson(jsonObject.get("openedCourse").toString(),courseListType);
+
+		return new Semester(semesterName, openedCourse));
+
+	}
+	
+	public Schedule initializeSchedule(JSONObject jsonObject){
+		
+		Gson gson = new Gson();
+		HashMap courseDayHour = jsonToMap(jsonObject.get("courseDayHour").toString());
+
+		return new Schedule(courseDayHour));
+
+	}
+	
+	public Lecturer initializeLecturer(JSONObject jsonObject){
+		
+		Gson gson = new Gson();
+		Type courseListType = new TypeToken<ArrayList<Student>>(){}.getType();
+		String lecturerName = jsonObject.get("name").toString();
+		String lecturerSurname = jsonObject.get("surname").toString();
+		Schedule schedule = jsonToClassSchedule(jsonObject.get("schedule").toString());
+		ArrayList<Student> givenCourses = gson.fromJson(jsonObject.get("givenCourses").toString(),courseListType);
+
+		return new Lecturer(lecturerName, lecturerSurname, schedule, givenCourses));
+
+	}
+	
+	public Advisor initializeAdvisor(JSONObject jsonObject){
+		
+		Gson gson = new Gson();
+		Type courseListType = new TypeToken<ArrayList<Student>>(){}.getType();
+		ArrayList<Student> advisedStudents = gson.fromJson(jsonObject.get("advisedStudents").toString(),courseListType);
+		String advisorName = jsonObject.get("name").toString();
+		String advisorSurname = jsonObject.get("surname").toString();
+
+		return new Advisor(advisedStudents, advisorName, advisorSurname));
 
 	}
 
@@ -111,63 +265,6 @@ public class FileManager {
 		return new Transcript(courseGrade,student,GPA,takenCredit,takenCourses,notTakenCourses);
 
 
-	}
-
-	public ArrayList<Course> readCourse(String path) {
-
-		ArrayList<Course> courses = new ArrayList<Course>();
-
-		Gson gson = new Gson();
-		String courseCode = jsonObject.get("coursecode").toString();
-		String courseName = jsonObject.get("coursename").toString();
-		Integer credit = jsonToClassTranscript(jsonObject.get("credit").toString());
-		Type courseListType1 = new TypeToken<ArrayList<Course>>(){}.getType();
-		ArrayList<Course> prerequisites = gson.fromJson(jsonObject.get("prerequisites").toString(),courseListType1);
-		Semester semester = jsonToClassSemester(jsonObject.get("semester").toString());
-		Lecturer lecturer = jsonToClassLecturer(jsonObject.get("lecturer").toString());
-		Type courseListType2 = new TypeToken<ArrayList<Student>>(){}.getType();
-		ArrayList<Course> courseStudents = gson.fromJson(jsonObject.get("courseStudents").toString(),courseListType2);
-
-		return courses.add(new Course(courseCode, courseName, credit, prerequisites, semester, lecturer, courseStudents));
-	}
-
-	public ArrayList<Lecturer> readLecturer(String path) throws Exception {
-
-		ArrayList<Lecturer> lecturers = new ArrayList<Lecturer>();
-
-		String lecturerName = jsonObject.get("name").toString();
-		String lecturerSurname = jsonObject.get("surname").toString();
-		Schedule schedule = jsonToClassSchedule(jsonObject.get("schedule").toString());
-		Type courseListType = new TypeToken<ArrayList<Course>>(){}.getType();
-		ArrayList<Course> givenCourses = gson.fromJson(jsonObject.get("givenCourses").toString(),courseListType);
-
-		return lecturers.add(new Lecturer(lecturerName, lecturerSurname, schedule, givenCourses))
-	}
-
-	public ArrayList<Advisor> readAdvisor(String path) {
-
-		ArrayList<Advisor> advisors = new ArrayList<Advisor>();
-
-		try {
-			Object obj = parser.parse(new FileReader(path));
-
-			JSONObject jsonObject = (JSONObject) obj;
-
-			String lecturerName = (String) jsonObject.get("name");
-
-			String lecturerSurname = (String) jsonObject.get("surname");
-
-			advisors.add(new Advisor());
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
-
-		return advisors;
 	}
 
 	public void writeToFile(String path, String message) {
