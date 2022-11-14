ec10287ba1d559590c05a0f8c22037599aa334a1
mertsaglam <mertsaglam@marun.edu.tr>
2022-11-14 16:44:48 +0300

    Advisor class edited

--- Java Project/Advisor.java
+++ Java Project/Advisor.java
@@ -1,3 +1,17 @@
-public class Advisor {
+import java.util.ArrayList;
 
+public class Advisor {
+//The students that are advised by the advisor
+private ArrayList<Student> advisedStudents;
+//The advisor's name
+private String advisorName;
+//The advisor's surname
+private String advisorSurname;
+//Constructor for advisor takes in an arraylist of students, name and surname
+    public Advisor(ArrayList<Student> advisedStudents, String advisorName, String advisorSurname) {
+        this.advisedStudents = advisedStudents;
+        this.advisorName = advisorName;
+        this.advisorSurname = advisorSurname;
+    }
 }
+
