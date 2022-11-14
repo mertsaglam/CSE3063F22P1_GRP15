5e57a8419ab0c2069f0b047ab192d1d60d0e85c1
mertsaglam <mertsaglam@marun.edu.tr>
2022-11-14 16:53:17 +0300

    .

--- Java Project/Course.java
+++ Java Project/Course.java
@@ -10,11 +10,60 @@ public abstract class Course {
 	protected Semester semester;
 	protected ArrayList<Student> courseStudents;
 	
+	//Constructor with no parameters
 	public Course() {
-	
+		this.courseCode = "";
+		this.courseName = "";
+		this.credit = 0;
+		this.prerequisites = new ArrayList<Course>();
+		this.schedule = new Schedule();
+		this.semester = new Semester();
+		this.courseStudents = new ArrayList<Student>();
 	}
-	
-	public Course getCourse(String courseCode) {
-		return this;
+	//Constructor with parameters takes in a course code, course name, credit, prerequisites, schedule, semester and course students
+	public Course(String courseCode, String courseName, int credit, ArrayList<Course> prerequisites, Schedule schedule, Semester semester, ArrayList<Student> courseStudents) {
+		this.courseCode = courseCode;
+		this.courseName = courseName;
+		this.credit = credit;
+		this.prerequisites = prerequisites;
+		this.schedule = schedule;
+		this.semester = semester;
+		this.courseStudents = courseStudents;
 	}
+	//Getters and setters for the properties
+	//Getter for course code property
+	public String getCourseCode() {
+		return courseCode;
+	}
+	//Getter for course name property
+	public String getCourseName() {
+		return courseName;
+	}
+	//Getter for credit property
+	public int getCredit() {
+		return credit;
+	}
+	//Getter for prerequisites property
+	public ArrayList<Course> getPrerequisites() {
+		return prerequisites;
+	}
+	//Getter for semester property
+	public Semester getSemester() {
+		return semester;
+	}
+	//Getter for schedule property
+	public Schedule getSchedule() {
+		return schedule;
+	}
+	//Getter for course students property
+	public ArrayList<Student> getCourseStudents() {
+		return courseStudents;
+	}
+	//Setter for course students property
+	public void setCourseStudents(ArrayList<Student> courseStudent) {
+		this.courseStudents = courseStudents;
+	}
+
+
+
 }
