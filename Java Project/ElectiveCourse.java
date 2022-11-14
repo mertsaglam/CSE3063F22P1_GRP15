3c327bdb330a16c3c2ea0d8e94eba28a40aa5f5f
ceerdonmez <117481993+ceerdonmez@users.noreply.github.com>
2022-11-14 20:50:58 +0300

    Update ElectiveCourse.java

--- Java Project/ElectiveCourse.java
+++ Java Project/ElectiveCourse.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 public class ElectiveCourse extends Course {
 	
 	private int quota;
+	private Schedule schedule;
 	private String electiveType;
 	
-	public ElectiveCourse(String courseCode, int credit, ArrayList<Course> prerequisites, Schedule schedule, Semester semester, int quota, String electiveType) {
+	public ElectiveCourse(String courseCode, String courseName,int credit, ArrayList<Course> prerequisites, Schedule schedule, Semester semester, Lecturer lecturer,int quota, String electiveType) {
 		this.courseCode = courseCode;
+		this.courseName = courseName;
 		this.credit = credit;
 		this.prerequisites = prerequisites;
 		this.schedule = schedule;
 		this.semester = semester;
+		this.lecturer = lecturer;
 		this.quota = quota;
 		this.electiveType = electiveType;
 	}
