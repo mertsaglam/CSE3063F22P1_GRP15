ee4cd25e756407c0d469c66ab19438be1ea3ba8d
memirutku <72553236+memirutku@users.noreply.github.com>
2022-11-15 00:52:10 +0300

    Update Schedule.java

--- Java Project/Schedule.java
+++ Java Project/Schedule.java
@@ -1,40 +1,43 @@
-//import java.util.ArrayList;
-//import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
  public class Schedule {
 
-    //private HashMap<String day,String[] startHour> courseDayHour =  new HashMap <>();
-     private HashMap<String,String[]> courseDayHour = new HashMap <String,String[]>();
+    //added ver1.0 private HashMap<String,String[]> courseDayHour = new HashMap <String,String[]>();
+     private HashMap<String,String[]> courseDayHour;
 
-
-
+    //added ver1.0
     public Schedule (HashMap<String,String[]> courseDayHour) {
        this.courseDayHour = courseDayHour;
-
-
+       
        //days and start hours
-        courseDayHour.put("Monday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Tuesday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Wednesday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Thursday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Friday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Saturday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
-        courseDayHour.put("Sunday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0 courseDayHour.put("Monday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0  courseDayHour.put("Tuesday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0 courseDayHour.put("Wednesday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0  courseDayHour.put("Thursday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0  courseDayHour.put("Friday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0  courseDayHour.put("Saturday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
+        //added ver1.0   courseDayHour.put("Sunday",new String[]{"8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"});
     }
+
+    
+    // added ver 2.0
+public Schedule (){
+      this.courseDayHour = new HashMap<String,String[]>();
+}
 
 
 //getter
 //HashMap<String day,String[] startHour>
-
-public HashMap<String ,String[]> getCourseDayHour(){
+//added ver 1.0
+public HashMap<String,String[]> getCourseDayHour(){
         return courseDayHour;
-    }
+}
+
 
     //setter
 //HashMap<String day,String[] startHour>
-public void setCourseDayHour(HashMap<String ,String[]>courseDayHour){
-
-        this.courseDayHour= courseDayHour;
+     //added ver1.0
+public void setCourseDayHour(HashMap<String,String[]>courseDayHour){
+        this.courseDayHour = courseDayHour;
 }
 }
