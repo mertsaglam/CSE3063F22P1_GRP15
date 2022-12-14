7cbe97a503d2a9e66f5d762e266a48e51cb6be1a
mertsaglam <mertsaglam@marun.edu.tr>
2022-12-14 15:57:01 +0300

    Dependency

--- Java Project/Iteration_2/src/Main.java
+++ Java Project/Iteration_2/src/Main.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         FileManager1 fileManager1 = new FileManager1();
-        ArrayList<Lecturer> lecturers = fileManager1.readLecturers("/Users/omerfarukbulut/CSE3063F22P1_GRP15/Java Project/Iteration_2/src/Jsons/lecturer.json");
-        ArrayList<CompulsoryCourse> courses = fileManager1.readCourse("/Users/omerfarukbulut/CSE3063F22P1_GRP15/Java Project/Iteration_2/src/Jsons/compulsoryCourses.json");
+        ArrayList<Lecturer> lecturers = fileManager1.readLecturers("Java Project/Iteration_2/src/Jsons/lecturer.json");
+        ArrayList<CompulsoryCourse> courses = fileManager1.readCourse("Java Project/Iteration_2/src/Jsons/compulsoryCourses.json");
         for (CompulsoryCourse course: courses)
             System.out.println(course.getCourseSection());
     }
