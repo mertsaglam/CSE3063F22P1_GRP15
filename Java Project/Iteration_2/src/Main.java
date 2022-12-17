427bd1d023370b9a310b77d1143452722d26252a
Tarık <117022474+tarikyumbul@users.noreply.github.com>
2022-12-17 17:55:24 +0300

    Update Main.java

--- Java Project/Iteration_2/src/Main.java
+++ Java Project/Iteration_2/src/Main.java
@@ -14,12 +14,21 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+       FileManager1 fileManager1 = new FileManager1();
+        ArrayList<CompulsoryCourse> courses = fileManager1.readCourse("Java Project/Iteration_2/src/Jsons/compulsoryCourses.json");
+        /* for(CompulsoryCourse course: courses){
+            String randomSemester = new Random().nextBoolean() ? "fall" : "spring";
+            course.setOpenedTerm(randomSemester);
+        }
+        JsonArray jsonArray = new Gson().toJsonTree(courses).getAsJsonArray();
 
+        File file = new File("course.json");
+        FileWriter fileWriter = new FileWriter(file,true);
+        fileWriter.write(jsonArray.toString());
+        fileWriter.close();*/
         CourseRegistrationSimulation courseRegistrationSimulation = new CourseRegistrationSimulation();
         courseRegistrationSimulation.starSimulation();
-        
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
+
 
     }
 
