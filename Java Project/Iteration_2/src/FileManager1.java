c924d837609bc3bacc28a434f1d1cd4848778408
ceerdonmez <117481993+ceerdonmez@users.noreply.github.com>
2022-12-17 17:43:33 +0300

    Update FileManager1.java

--- Java Project/Iteration_2/src/FileManager1.java
+++ Java Project/Iteration_2/src/FileManager1.java
@@ -75,19 +75,29 @@ public class FileManager1 {
     }
 
     public void writeToFile(Student student, String filePath) throws IOException {
-        File file = new File(filePath);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        FileWriter fileWriter = new FileWriter(file, true);
-        fileWriter.write(gson.toJson(student));
-        fileWriter.close();
+    	try {
+    		File file = new File(filePath);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(gson.toJson(student));
+            fileWriter.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+        
     }
 
     public void writeToFileWithArray(ArrayList<Student> student, String filePath) throws IOException {
-        File file = new File(filePath);
-        Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
-        FileWriter fileWriter = new FileWriter(file, false);
-        fileWriter.write(gson.toJson(student));
-        fileWriter.close();
+    	try {
+    		File file = new File(filePath);
+            Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
+            FileWriter fileWriter = new FileWriter(file, false);
+            fileWriter.write(gson.toJson(student));
+            fileWriter.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+        
     }
 
 }
