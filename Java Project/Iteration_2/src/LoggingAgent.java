2cfee537f1d8d88f70b32176d475d324e00fbfcf
memirutku <72553236+memirutku@users.noreply.github.com>
2022-12-17 17:08:07 +0300

    Update LoggingAgent.java

--- Java Project/Iteration_2/src/LoggingAgent.java
+++ Java Project/Iteration_2/src/LoggingAgent.java
@@ -1,1 +1,11 @@
+package Iteration_2.src;
 
+import org.apache.logging.log4j.*;
+
+
+public class LoggingAgent {
+    
+    public void LoggerMethod() {
+        Logger logger = LogManager.getLogger();
+        logger.info(getMethodName(1));
+    }
