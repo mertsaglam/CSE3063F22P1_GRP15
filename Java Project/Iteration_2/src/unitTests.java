3b5945e36acda039b7066dbcbdd6278e28060a3e
ceerdonmez <117481993+ceerdonmez@users.noreply.github.com>
2022-12-17 17:51:44 +0300

    Add files via upload

---
+++ Java Project/Iteration_2/src/unitTests.java
@@ -0,0 +1,23 @@
+package a;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class unitTests {
+	
+	@Test
+	public void testAssertions() throws Exception {
+		
+		CourseRegistrationSimulation c = new CourseRegistrationSimulation();
+		c.createCourses();
+		
+		ArrayList<Student> students = c.createRandomStudent(5);
+		
+		assertTrue(students.size()==5);
+		assertNotNull(students);
+	}
+
+}
