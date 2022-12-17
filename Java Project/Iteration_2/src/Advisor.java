7c39cfa69c78b2a88f8cba3295e0db9f0d85851b
ubeydetuna <76559333+ubeydetuna@users.noreply.github.com>
2022-12-17 17:18:58 +0300

    Update Advisor.java

--- Java Project/Iteration_2/src/Advisor.java
+++ Java Project/Iteration_2/src/Advisor.java
@@ -14,7 +14,7 @@ public class Advisor extends Lecturer {
 
     public Advisor(String lecturerName, String lecturerSurname, Schedule schedule, ArrayList<String> students) {
         super(lecturerName, lecturerSurname, schedule);
-        this.students = students;
+        this.students = ;
     }
 
     public Advisor() {
