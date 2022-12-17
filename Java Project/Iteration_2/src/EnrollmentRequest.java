ae0ce912b7cc1c79ab4d10543e23a310d4b76299
Tarık <117022474+tarikyumbul@users.noreply.github.com>
2022-12-17 17:54:20 +0300

    Update EnrollmentRequest.java

--- Java Project/Iteration_2/src/EnrollmentRequest.java
+++ Java Project/Iteration_2/src/EnrollmentRequest.java
@@ -17,76 +17,52 @@ public class EnrollmentRequest {
     public EnrollmentRequest(ArrayList<CompulsoryCourse> courses, Student student) {
         this.courses = courses;
         this.student = student;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public ArrayList<CompulsoryCourse> getCourses() {
         return courses;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setCourses(ArrayList<CompulsoryCourse> courses) {
         this.courses = courses;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public HashMap<String, String> getResult() {
         return result;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setResult(HashMap<String, String> result) {
         this.result = result;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public Student getStudent() {
         return student;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setStudent(Student student) {
-        this.student = student;        
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
+        this.student = student;
     }
 
     public HashMap<String, Schedule> getSchedule() {
         return schedule;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setSchedule(HashMap<String, Schedule> schedule) {
         this.schedule = schedule;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void appendResult(HashMap<String, String> result1) {
         HashMap<String, String> temp = this.result;
         temp.putAll(result1);
         setResult(temp);
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public int getCreditLimit() {
         return creditLimit;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setCreditLimit(int creditLimit) {
         this.creditLimit = creditLimit;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public int getTotalCredit() {
@@ -95,20 +71,14 @@ public class EnrollmentRequest {
             credit += course.getCredit();
         }
         return credit;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public String getError() {
         return error;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
     public void setError(String error) {
         this.error = error;
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 
 
@@ -122,7 +92,5 @@ public class EnrollmentRequest {
                 ", creditLimit=" + creditLimit +
                 ", error='" + error + '\'' +
                 '}';
-        Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
-        LoggingAgent.LoggerMethod();
     }
 }
