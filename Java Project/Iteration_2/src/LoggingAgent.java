e5602e8d71f07a6cd7c136c0a92f212a032a48aa
Tarık <117022474+tarikyumbul@users.noreply.github.com>
2022-12-17 17:09:02 +0300

    Update LoggingAgent.java

--- Java Project/Iteration_2/src/LoggingAgent.java
+++ Java Project/Iteration_2/src/LoggingAgent.java
@@ -2,10 +2,16 @@ package Iteration_2.src;
 
 import org.apache.logging.log4j.*;
 
-
 public class LoggingAgent {
     
     public void LoggerMethod() {
         Logger logger = LogManager.getLogger();
         logger.info(getMethodName(1));
     }
+
+	public static String getMethodName(final int depth){
+		final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		return ("Method '" + ste[ste.length - 1 - depth].getMethodName() + "' has been executed.");
+	}
+
+}
