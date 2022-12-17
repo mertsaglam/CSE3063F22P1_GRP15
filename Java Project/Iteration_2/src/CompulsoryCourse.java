c8d60b10b7e2cb6f7ed054f65f4acaa26caa5e95
ErkamFidan <45895812+terkamfdn@users.noreply.github.com>
2022-12-17 17:41:04 +0300

    Update CompulsoryCourse.java

--- Java Project/Iteration_2/src/CompulsoryCourse.java
+++ Java Project/Iteration_2/src/CompulsoryCourse.java
@@ -48,65 +48,95 @@ public  class CompulsoryCourse {
 
 	public String getCourseName() {
 		return courseName;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public int getCredit() {
 		return credit;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setCredit(int credit) {
 		this.credit = credit;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setPrerequisites(ArrayList<String> prerequisites) {
 		this.prerequisites = prerequisites;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setLecturer(Lecturer lecturer) {
 		this.lecturer = lecturer;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public String getOpenedTerm() {
 		return openedTerm;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setOpenedTerm(String openedTerm) {
 		this.openedTerm = openedTerm;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public ArrayList<String> getPrerequisites() {
 		return prerequisites;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 	//Getter for semester property
 	//Getter for schedule property
 	public Lecturer getLecturer() {
 		return lecturer;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 	//Getter for course students property
 	public ArrayList<Student> getCourseStudents() {
 		return courseStudents;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 	//Setter for course students property
 	public void setCourseStudents(ArrayList<Student> courseStudent) {
 		this.courseStudents = courseStudents;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public CourseSection getCourseSection() {
 		return courseSection;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setCourseSection(CourseSection courseSection) {
 		this.courseSection = courseSection;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 	public Schedule getRandomSection(){
 		int size =this.getCourseSection().getSchedule().size();
 		HashMap<String,Schedule> scheduleHashMap = this.getCourseSection().getSchedule();
 		Random random = new Random();
 		return size == 1 ? scheduleHashMap.get(String.valueOf(1)):scheduleHashMap.get(String.valueOf(random.nextInt(1,size)));
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 
 	}
 
@@ -122,6 +152,8 @@ public  class CompulsoryCourse {
 				", courseStudents=" + courseStudents +
 				", givenSemester='" + openedTerm + '\'' +
 				'}';
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 }
 class CourseSection {
@@ -136,50 +168,74 @@ class CourseSection {
 		this.sectionCount = sectionCount;
 		this.schedule = schedule;
 		this.quota = quota;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public CourseSection() {
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public String getCourse() {
 		return courseCode;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setCourse(String courseCode) {
 		this.courseCode = courseCode;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public int getSectionID() {
 		return sectionCount;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setSectionCount(int sectionCount) {
 		this.sectionCount = sectionCount;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 
 	public String getCourseCode() {
 		return courseCode;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setCourseCode(String courseCode) {
 		this.courseCode = courseCode;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public int getSectionCount() {
 		return sectionCount;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public HashMap<String, Schedule> getSchedule() {
 		return schedule;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setSchedule(HashMap<String, Schedule> schedule) {
 		this.schedule = schedule;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public int getQuota() {
 		return quota;
+		Iteration_2.src.LoggingAgent LoggingAgent = new Iteration_2.src.LoggingAgent();
+        	LoggingAgent.LoggerMethod();
 	}
 
 	public void setQuota(int quota) {
