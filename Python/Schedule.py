7fee7a47299608add20b4f907cea167d7aac61a6
memirutku <72553236+memirutku@users.noreply.github.com>
2023-01-08 15:17:15 +0300

    Create Schedule.py

---
+++ Python/Schedule.py
@@ -0,0 +1,32 @@
+class Schedule :
+    # (not sure)
+    # courseDayHour = None
+
+    def __init__(self, courseDayHour) :
+        self.__courseDayHour = courseDayHour
+
+    # (not sure)
+    def __init__(self) :
+        self.__courseDayHour =  dict()
+
+    def  getCourseDayHour(self) :
+        return self.__courseDayHour
+
+
+    def setCourseDayHour(self, courseDayHour) :
+        self.__courseDayHour = courseDayHour
+    def  toString(self) :
+        return "Schedule{" + "courseDayHour=" + str(self.__courseDayHour) + str('}')
+    def  isCollided(self, schedule,  schedule1) :
+        for key,value in schedule.getCourseDayHour().items() :
+            if ((key in schedule1.getCourseDayHour().keys())) :
+                for value in schedule.getCourseDayHour().get(key) :
+                    if (value  in schedule1.getCourseDayHour().get(key)) :
+                        return True
+        return False
+
+    def  appendSchedule(self, schedule,  schedule1) :
+        currentSchedule = self.__courseDayHour
+        schedule.__courseDayHour.putAll(schedule1.__courseDayHour)
+        self.__courseDayHour = schedule.__courseDayHour
+        return schedule
