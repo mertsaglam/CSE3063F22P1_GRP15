7047692e2adb928ad8ac0bf0fe9f9335ccfc38dc
sefengineer <103127021+sefengineer@users.noreply.github.com>
2023-01-08 17:57:09 +0300

    Update Schedule.py

--- Python/Schedule.py
+++ Python/Schedule.py
@@ -18,6 +18,10 @@ class Schedule :
    #toString to __str__     
    def  __str__(self) :
         return "Schedule{" + "courseDayHour=" + str(self.__courseDayHour) + str('}')
+        
+    # function to check whether there is a collision, a clash 
+    #in schedule 
+    
     def  isCollided(self, schedule,  schedule1) :
         for key,value in schedule.getCourseDayHour().items() :
             if ((key in schedule1.getCourseDayHour().keys())) :
@@ -25,7 +29,9 @@ class Schedule :
                     if (value  in schedule1.getCourseDayHour().get(key)) :
                         return True
         return False
-
+        
+    #appending the schedule
+    
     def  appendSchedule(self, schedule,  schedule1) :
         currentSchedule = self.__courseDayHour
         schedule.__courseDayHour.putAll(schedule1.__courseDayHour)
