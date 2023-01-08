7ddbf09458377e6b3899eee73b3e6396ef2ed352
ceerdonmez <117481993+ceerdonmez@users.noreply.github.com>
2023-01-09 01:37:37 +0300

    Main class added

---
+++ Python/src/Main.py
@@ -0,0 +1,15 @@
+import CourseRegistrationSimulation
+import FileManager1
+import os
+import random
+
+def main():
+    file_manager1 = FileManager1()
+    courses = file_manager1.read_course("Java Project/Iteration_2/src/Jsons/compulsoryCourses.json")
+    os.environ["log4j.configurationFile"] = "/Users/omerfarukbulut/CSE3063F22P1_GRP15/Java Project/Iteration_2/src/Resources/log4j.tld"
+    course_registration_simulation = CourseRegistrationSimulation()
+
+    course_registration_simulation.star_simulation()
+
+if __name__ == "__main__":
+    main()
